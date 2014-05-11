18f99a7f3b758c579155aa815b0328ec40b2ea50
Andy Kuszyk <andykuszyk@live.co.uk>
2014-05-11 10:30:44 +0000

    Problem 4 in progress,

--- ProjectEuler.py
+++ ProjectEuler.py
@@ -1,45 +1,40 @@
-print "Enter the problem number:"
-
-# Problem 4 - Largest palindrome
+import time
 
 def IsPalindrome(testNumber):
     testString = str(testNumber)
-
-    # Test for even length
-    if len(testString)%2 <>0:
-        return False
-
-    
-    # Loop through characters until middle and match to end
     char=0
     while char< len(testString)/2:
         if testString[char] <> testString[len(testString)-char-1]:
             return False
         char+=1
-
     return True
 
-largestPalindrome=0
-numberOne=999
-numberTwo=999
+def IsAFactorOfThreeDigitNumbers(testNumber):
+	a=999
+	while a>99:
+		if testNumber%a<>0:
+			continue
+		b=testNumber/a
+		if len(str(a))==3 and len(str(b))==3 and int(b)==b:
+			return True
+		a-=1
+	return False
 
-while numberOne>99:
-    if largestPalindrome>0:
-        break
-    
-    while numberTwo>99:
-        print numberOne*numberTwo
-        if IsPalindrome(numberOne * numberTwo):
-            largestPalindrome=numberOne * numberTwo
-            break
-        
-        numberTwo-=1
-        
-    numberOne-=1
-    numberTwo=numberOne
+#startTime=time.time()
+#result=IsPalindrome(123321)
+#endTime= time.time()
+#print result 
+#print endTime-startTime
 
-if largestPalindrome>0:
-    print "Largest palindrome found: " + str(largestPalindrome)
-else:
-    print "No palindrome found."
+a=999
+b=999
+testNumber = a*b
+minNumber=100*100
+while testNumber>minNumber:
+	if IsPalindrome(testNumber) and IsAFactorOfThreeDigitNumbers(testNumber):
+		break
+	testNumber-=1
 
+print testNumber
+
+#print time.time()-startTime
